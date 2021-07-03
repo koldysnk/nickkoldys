eba022a7ccd4cde089422d23f8ca96cec64af176
Nick Koldys <koldysnk@dukes.jmu.edu>
2021-07-02 20:14:10 -0400

    upload changes

--- nickkoldys/src/PixelPerfectChoose.js
+++ nickkoldys/src/PixelPerfectChoose.js
@@ -2,9 +2,32 @@ import React, { useContext, useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startPP, startPPTri, stepPP, stepPPTri, setPPBestData, setPPCurrAccuracy, setPPCurrData, setPPGenerationCount, setPPAccuracy, setPPChoosePicture, setPPActivePicture } from './actions';
 import './PixelPerfectChoose.css';
+import {useDropzone} from 'react-dropzone'
 
 export function PixelPerfectChoose(props) {
     const dispatch = useDispatch();
+
+    const [message, setMessage] = useState('Click or Drop to upload.')
+
+
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: 'image/*',
+        onDrop: (acceptedFiles) => {
+            if (!acceptedFiles[0]){
+                setMessage('Click or Drop to upload. (Must be an Image)')
+            } else{
+                let url = URL.createObjectURL(acceptedFiles[0])
+                dispatch(setPPActivePicture(url))
+                dispatch(setPPChoosePicture(false))
+            }
+
+        }
+    })
+    
+    const selectWhale = () => {
+        dispatch(setPPActivePicture('whale.jpg'))
+        dispatch(setPPChoosePicture(false))
+    }
     
     const selectPanda = () => {
         dispatch(setPPActivePicture('panda.jpg'))
@@ -26,12 +49,30 @@ export function PixelPerfectChoose(props) {
         dispatch(setPPChoosePicture(false))
     }
 
+    const selectEyes = () => {
+        dispatch(setPPActivePicture('eyes.jpg'))
+        dispatch(setPPChoosePicture(false))
+    }
+
+    const close = () => {
+        dispatch(setPPChoosePicture(false))
+    }
+
     return (
         <div className='PixelPerfectChoose' >
-            <img className='ppChooseImg' onClick={selectPanda} src={'panda.jpg'}></img>
-            <img className='ppChooseImg' onClick={selectCol} src={'colosseum.jpg'}></img>
-            <img className='ppChooseImg' onClick={selectDino} src={'dino.jpg'}></img>
-            <img className='ppChooseImg' onClick={selectUnder} src={'underwater.jpg'}></img>
+            <div className='ppImages'>
+                <img className='ppChooseImg' onClick={selectPanda} src={'panda.jpg'}></img>
+                <img className='ppChooseImg' onClick={selectCol} src={'colosseum.jpg'}></img>
+                <img className='ppChooseImg' onClick={selectDino} src={'dino.jpg'}></img>
+                <img className='ppChooseImg' onClick={selectUnder} src={'underwater.jpg'}></img>
+                <img className='ppChooseImg' onClick={selectWhale} src={'whale.jpg'}></img>
+                <img className='ppChooseImg' onClick={selectEyes} src={'eyes.jpg'}></img>
+            </div>
+            <div className='ppUpload' {...getRootProps()}>
+                <input {...getInputProps()}></input>
+                <p>{message}</p>
+            </div>
+            <img className='ppChooseClose' src='x.png' onClick={close}></img>
         </div>
     );
 }
