fd5c430b275af3771ae8bbdf18d3cf529a280cb9
Nick Koldys <koldysnk@dukes.jmu.edu>
2021-07-07 20:54:47 -0400

    window size eval

--- nickkoldys/src/MazeSolver.js
+++ nickkoldys/src/MazeSolver.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRandomIntInclusive } from './actions';
 import './MazeSolver.css';
+import {useWindowDimensions} from './windowInfo';
 
 export function MazeSolver(props) {
     const activePicture = useSelector(state => state.ppActivePicture);
@@ -12,9 +13,12 @@ export function MazeSolver(props) {
 
     // Use useRef for mutable variables that we want to persist
     // without triggering a re-render on their change
+    const { height, width } = useWindowDimensions();
+    const maxWidth = Math.floor(width*.8/10)
+    const maxHeight = Math.floor(height*.8/10)
     const maze = useRef([])
-    const width = useRef(30)
-    const height = useRef(30)
+    const currWidth = useRef(10)
+    const currHeight = useRef(10)
 
     const dispatch = useDispatch();
 
@@ -23,16 +27,16 @@ export function MazeSolver(props) {
         if(s.length>0){
             let x = s[s.length-1].x
             let y = s[s.length-1].y
-            let numSides = (y<(width.current-1) && !maze.current[y+1][x].visited) +
+            let numSides = (y<(currWidth.current-1) && !maze.current[y+1][x].visited) +
                 (y>0 && !maze.current[y-1][x].visited) +
-                (x<(height.current-1) && !maze.current[y][x+1].visited) +
+                (x<(currHeight.current-1) && !maze.current[y][x+1].visited) +
                 (x>0 && !maze.current[y][x-1].visited) 
 
             if(numSides){
                 let rand = Math.floor(Math.random()*numSides)
 
                 //Right to Left
-                if(y<(width.current-1) && !maze.current[y+1][x].visited){
+                if(y<(currWidth.current-1) && !maze.current[y+1][x].visited){
                     if(rand==0){
                         maze.current[y+1][x].visited = true
                         maze.current[y+1][x].l = false
@@ -44,7 +48,7 @@ export function MazeSolver(props) {
                 }
 
                 //Top to Bottom
-                if(x<(height.current-1) && !maze.current[y][x+1].visited){
+                if(x<(currHeight.current-1) && !maze.current[y][x+1].visited){
                     if(rand==0){
                         maze.current[y][x+1].visited = true
                         maze.current[y][x+1].t = false
@@ -89,17 +93,17 @@ export function MazeSolver(props) {
     const initializeMaze = () => {
         let newMaze = []
 
-        for (let i = 0; i < width.current; i++) {
+        for (let i = 0; i < currWidth.current; i++) {
             let row = []
-            for (let j = 0; j < height.current; j++) {
+            for (let j = 0; j < currHeight.current; j++) {
                 row.push({x:j,y:i, t:true,l:true,r:true,b:true,visited:false})
             }
             newMaze.push(row)
         }
         maze.current = newMaze
 
-        let x = Math.floor(Math.random()*width.current)
-        let y = Math.floor(Math.random()*height.current)
+        let x = Math.floor(Math.random()*currWidth.current)
+        let y = Math.floor(Math.random()*currHeight.current)
         maze.current[x][y].visited = true
 
         let s = [maze.current[x][y]]
@@ -121,6 +125,7 @@ export function MazeSolver(props) {
             <div>
                 <button onClick={initializeMaze}>Generate</button>
             </div>
+            <input type="range" min="10" max={maxHeight} value={currWidth} class="slider" id="myRange"></input>
             <div className='msBoard'>
                 {maze.current.map((w, i) => {
                     return <div key={i}>{w.map((v, j) => {
@@ -128,7 +133,7 @@ export function MazeSolver(props) {
 
                         if(v.x==0 &&v.y==0){
                             className+= ' msStart'
-                        }else if(v.x==width.current-1 &&v.y==height.current-1){
+                        }else if(v.x==currWidth.current-1 &&v.y==currHeight.current-1){
                             className+= ' msEnd'
                         }
 
