de9c40c3e6d7a23f60eee13f267ea0c4c5935ceb
Nick Koldys <koldysnk@dukes.jmu.edu>
2023-01-12 17:01:20 -0500

    tile style

--- nickkoldys_v2/src/food-planner/RecipeTile.js
+++ nickkoldys_v2/src/food-planner/RecipeTile.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './RecipeTile.css';
 export function RecipeTile(props) {
-
+    const description = 'This is a sample description of the recipe.'
     return (
-        <div>
-            {props.name}
+        <div className="Recipe-Tile">
+            <div className="Recipe-Tile-Image"></div>
+            <div className="Recipe-Tile-Text">
+                <h4>{props.name}</h4>
+                <p>{description}</p>
+            </div>
+            <div className="Recipe-Tile-Star"></div>
         </div>
     );
 }
