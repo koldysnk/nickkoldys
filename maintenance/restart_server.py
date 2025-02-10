9acb8ea49743b07655c97c73b7928a958992db49
koldysnk <koldysnk@dukes.jmu.edu>
2025-02-10 18:48:25 +0000

    update maintenance script

--- maintenance/restart_server.py
+++ maintenance/restart_server.py
@@ -1,16 +1,61 @@
-from datetime import date
 import os
+import requests
+import traceback
+import subprocess
+from datetime import date
+from requests.exceptions import SSLError
+
+def output(value,f=None):
+        print(value)
+        if f:
+                f.write(f'{value}\n')
 
 def main():
-	todays_date = date.today()
-	log_file = f"log_restart_server_{todays_date.year}-{todays_date.month}-{todays_date.day}.txt"
-	print(log_file)
+        todays_date = date.today()
+        location = "/home/nkoldys/personalSite/nickkoldys"
+        log_file = f"{location}/maintenance/log_restart_server_{todays_date.year}-{todays_date.month}-{todays_date.day}.txt"
+        output(log_file)
 
-	os.system(f"cd ../server/; ./node_modules/.bin/pm2 stop server >> ../maintenance/{log_file}")
-	os.system(f"cd ../server/; ./node_modules/.bin/pm2 start --name server app.js >> ../maintenance/{log_file}")
+        try:
+                f = open(log_file,"w")
+                f.write("Starting Restart Process...\n")
 
-	with open(log_file) as f:
-		if "Process successfully started" in f.read():
-			print("Process successfully started - removing log file")
-			os.remove(log_file)
+
+                #validate if srver is online and restart if needed
+                status = subprocess.run([f'{location}/server/node_modules/.bin/pm2','status'],capture_output=True,text=True)
+                #output(status.stdout,f)
+                if "online" in status.stdout:
+                        output("Server is online.",f)
+                else:
+                        output("Server is offline and needs to be restarted",f)
+                        restart = subprocess.run([f'{location}/server/node_modules/.bin/pm2','start','--name','server'],capture_output=True,text=True)
+                        output(restart.stdout,f)
+
+                        status2 = subprocess.run([f'{location}/server/node_modules/.bin/pm2','status'],capture_output=True,text=True)
+                        output(status2.stdout,f)
+                        if "online" in status2.stdout:
+                                output("Server was sucessfully restarted.",f)
+                        else:
+                                output("SERVER FAILED TO RESTART.",f)
+
+                #validate connection to endpoint
+                try:
+                        response = requests.get("https://server.nickkoldys.com:8442/allwords/0/1")
+                        if response.ok:
+                                output("The connection was sucessfull",f)
+                        else:
+                                outpur("Something else is wrong... This needs to be investigated.",f)
+                except SSLError as e:
+                        output(e)
+                        output("The server certification needs to be refreshed")
+
+
+        except BaseException as e:
+                output(e,f)
+                output(traceback.format_exc(),f)
+        else:
+                output("Process concluded with no errors.",f)
+        finally:
+                f.close()
+
 main()
